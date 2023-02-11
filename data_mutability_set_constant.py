5b02f450bf1e4147c84155ebb7dd93b6c1b53737
Swyter <swyterzone@gmail.com>
2023-02-12 00:34:54 +0100

    Make the mutability script work when there's just a single line highlighted, as well as when we've selected multiple of them.

--- data_mutability_set_constant.py
+++ data_mutability_set_constant.py
@@ -10,11 +10,34 @@ from ghidra.program.model.data import MutabilitySettingsDefinition
 # swy: thanks to kotcrab for ghidra-allegrex and this example, otherwise I'd still be lost in the rat's nest of Java uglyness:
 #      https://programtalk.com/java-more-examples/ghidra.program.model.data.MutabilitySettingsDefinition.CONSTANT/
 
-mutab = getDataAt(currentAddress).getLong(MutabilitySettingsDefinition.MUTABILITY)
+# mutab = getDataAt(currentAddress).getLong(MutabilitySettingsDefinition.MUTABILITY)
+# 
+# if mutab:
+# 	print("[i] Current mutability setting is %u; setting %#x to constant " % (mutab, int(currentAddress.toString(), 16)))
 
-if mutab:
-	print("[i] Current mutability setting is %u; setting %#x to constant " % (mutab, int(currentAddress.toString(), 16)))
+# swy: make it work when there's just a single line highlighted,
+#      as well as when we've selected multiple of them.
+range = [currentAddress, currentAddress.add(1)]
 
-getDataAt(currentAddress).setLong(
-	MutabilitySettingsDefinition.MUTABILITY, MutabilitySettingsDefinition.CONSTANT
-)
+if currentSelection:
+	range = [currentSelection.minAddress, currentSelection.maxAddress]
+
+prev_set_data = None; cur_addr = range[0]
+while cur_addr < range[1]:
+	# print(cur_addr, range[1])
+	cur_data = getDataAt(cur_addr)
+	cur_addr = cur_addr.add(1)
+
+	if not cur_data:
+		continue
+
+	# swy: avoid setting the same object multiple times
+	#      as we swim through contiguous ranges
+	if prev_set_data == cur_data:
+		continue
+
+	cur_data.setLong(
+		MutabilitySettingsDefinition.MUTABILITY, MutabilitySettingsDefinition.CONSTANT
+	)
+	prev_set_data = cur_data
+	
