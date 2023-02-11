09c002a2853cff540102645735924e2717aab912
Swyter <swyterzone@gmail.com>
2023-02-11 13:08:12 +0100

    Found how to go from addresses to integers and vice versa, found how to find the memory block from an address. Boy, they don't make this easy.

--- metrowerks_rtti.py
+++ metrowerks_rtti.py
@@ -5,6 +5,27 @@
 #@menupath 
 #@toolbar 
 
+
+'''
+>>> currentProgram.getMemory().getBlock(currentProgram.getAddressFactory().getAddress("0x8080b600")).isExecute()
+False
+>>> currentProgram.getMemory().getBlock(currentProgram.getAddressFactory().getAddress("0x80799500")).isExecute()
+True
+'''
+
+# https://github.com/ghidraninja/ghidra_scripts/blob/master/golang_renamer.py
+addressFactory = currentProgram.getAddressFactory()
+
+def addressToInt(ghidra_addr):
+    return int(ghidra_addr.toString(), 16)
+
+def intToAddress(addr):
+    return addressFactory.getAddress(hex(addr))
+
+blocks = currentProgram.getMemory().getBlocks()
+#for block in blocks:
+#	print("Name: {}, Size: {}".format(block.getName(), block.getSize()))
+
 # https://github.com/NationalSecurityAgency/ghidra/issues/1969#issuecomment-1221655969
 def getUByte(address):  return getByte(address)  & 0xFF
 def getUShort(address): return getShort(address) & 0xFFFF
@@ -16,7 +37,7 @@ def getULong(address):  return getLong(address)  & 0xFFFFFFFFFFFFFFFF
 dt = getDataTypes("pointer")[0]
 addr = currentAddress;
 i = 0
-while getInt(addr) != 0 or addr <= currentAddress.add(4 + 4):
+while getUInt(addr) != 0 or addr <= currentAddress.add(4 + 4):
 	print("%X" % getUInt(addr))
 	clearListing(addr, addr.add(4))
 	createData(addr, dt);
