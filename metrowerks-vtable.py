0df328ad8d8c70a2566e39c1a79dc0ff64908a3b
Swyter <swyterzone@gmail.com>
2023-02-11 11:35:42 +0100

    Start learning to use the Ghidra FlatProgramAPI. Kind of messy, and not many examples around: https://ghidra.re/ghidra_docs/api/ghidra/program/flatapi/FlatProgramAPI.html#getSymbolAfter(ghidra.program.model.symbol.Symbol)https://github.com/HackOvert/GhidraSnippets#using-the-flatprogramapi
    https://en.wikipedia.org/wiki/Metrowerks
    https://reverseengineering.stackexchange.com/questions/30567/ghidrahow-to-set-global-variable-type
    https://github.com/NationalSecurityAgency/ghidra/issues/1986
    https://reverseengineering.stackexchange.com/questions/28039/access-data-at-memory-address-that-is-part-of-a-struct
    https://github.com/NationalSecurityAgency/ghidra/issues/2064

---
+++ metrowerks-vtable.py
@@ -0,0 +1,16 @@
+#TODO write a description for this script
+#@author 
+#@category _NEW_
+#@keybinding 
+#@menupath 
+#@toolbar 
+
+
+#TODO Add User Code Here
+
+dt = getDataTypes("pointer")[0]
+addr = currentAddress;
+
+clearListing(addr, addr.add(4))
+createData(addr, dt);
+createData(addr.add(4), dt);
