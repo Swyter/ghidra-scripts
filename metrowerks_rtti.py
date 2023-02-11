bea69a73cfb9fa4cd8a4dfb516663641fc11dbea
Swyter <swyterzone@gmail.com>
2023-02-11 12:15:08 +0100

    Start working out the fact that Java unsigned support sucks and we need some wrappers.

--- metrowerks_rtti.py
+++ metrowerks_rtti.py
@@ -5,6 +5,11 @@
 #@menupath 
 #@toolbar 
 
+# https://github.com/NationalSecurityAgency/ghidra/issues/1969#issuecomment-1221655969
+def getUByte(address):  return getByte(address)  & 0xFF
+def getUShort(address): return getShort(address) & 0xFFFF
+def getUInt(address):   return getInt(address)   & 0xFFFFFFFF
+def getULong(address):  return getLong(address)  & 0xFFFFFFFFFFFFFFFF
 
 #TODO Add User Code Here
 
@@ -12,6 +17,7 @@ dt = getDataTypes("pointer")[0]
 addr = currentAddress;
 i = 0
 while getInt(addr) != 0 or addr <= currentAddress.add(4 + 4):
+	print("%X" % getUInt(addr))
 	clearListing(addr, addr.add(4))
 	createData(addr, dt);
 	addr = addr.add(4); i+=1
