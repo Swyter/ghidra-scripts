a4b14cf8fa0425b6505c7fbc39f8d1951bee8c92
Swyter <swyterzone@gmail.com>
2023-02-12 00:11:37 +0100

    Improve the mutability-setting script. Add comments, log the previous state and clean it up.

--- data_mutability_set_constant.py
+++ data_mutability_set_constant.py
@@ -1,5 +1,5 @@
 #Allows you to quickly set the data/string at the selected address as constant, to show the string value in the decompilation.
-#@author Swyter <swyterzone+ghidra>
+#@author Swyter <swyterzone+ghidra@gmail.com>
 #@category Data
 #@keybinding Ctrl K
 #@menupath 
@@ -7,7 +7,14 @@
 
 from ghidra.program.model.data import MutabilitySettingsDefinition
 
-# swy: thanks to kotcrab for ghidra-allegrex and this example:
+# swy: thanks to kotcrab for ghidra-allegrex and this example, otherwise I'd still be lost in the rat's nest of Java uglyness:
 #      https://programtalk.com/java-more-examples/ghidra.program.model.data.MutabilitySettingsDefinition.CONSTANT/
-getDataAt(currentAddress).getLong(MutabilitySettingsDefinition.MUTABILITY);
-getDataAt(currentAddress).setLong(MutabilitySettingsDefinition.MUTABILITY, MutabilitySettingsDefinition.CONSTANT);
+
+mutab = getDataAt(currentAddress).getLong(MutabilitySettingsDefinition.MUTABILITY)
+
+if mutab:
+	print("[i] Current mutability setting is %u; setting %#x to constant " % (mutab, int(currentAddress.toString(), 16)))
+
+getDataAt(currentAddress).setLong(
+	MutabilitySettingsDefinition.MUTABILITY, MutabilitySettingsDefinition.CONSTANT
+)
